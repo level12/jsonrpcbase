61ca1f27e0618c2c588bd5b7957ef430a893d6a2
fuzzybyte <fuzzybyte@gmail.com>
2010-07-25 23:17:56 +0300

    Forgot to include the setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='jsonrpcbase',
+      version='0.1-dev',
+      description='Simple JSON-RPC service without transport layer',
+      author='Juhani Åhman',
+      author_email='juhani.ahman@cs.helsinki.fi',
+      url='http://bitbucket.org/fuzzybyte/jsonrpcbase/src',
+      py_modules=['jsonrpcbase'],
+     )
